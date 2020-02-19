61c2cab9bf2396bb29c39b6c87a46f5af3479b80
Tamara_PC\Tamara <tamara.awadieh@gmail.com>
2020-02-19 20:17:39 +0200

    first get user by id

--- model/users.js
+++ model/users.js
@@ -1,10 +1,27 @@
+const csv = require('fast-csv');
+const fs = require('fs');
+
+
 module.exports = {
     getUserById: async function(id){
         console.log(`getUserById called with id: ${id}`);
+        
+        const fileStream = fs.createReadStream("data.csv");
+        const parser = csv.parse();
+        
+        fileStream
+            .pipe(parser)
+            .on('error', error => console.error(error))
+            .on('readable', () => {
+               for (let row = parser.read(); row; row = parser.read()) {
+                    if(row[0] == id){
+                        console.log("found");
+                        return JSON.stringify(row);
+                    }
+                }
+            })
+            .on('end', (rowCount) => console.log(`Parsed ${rowCount} rows`));
 
-        // Add implementation here
-
-        return {};
     },
 
     getUsersByAge: async function(age) {
